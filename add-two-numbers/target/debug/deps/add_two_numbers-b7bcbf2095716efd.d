97b66fb43568861d587525b978151e04e9a8169e
KadoBOT <ricardo.ambrogi@gmail.com>
2020-08-01 18:51:52 +0200

    add_two_numbers

--- add-two-numbers/target/debug/deps/add_two_numbers-b7bcbf2095716efd.d
+++
@@ -1,5 +0,0 @@
-/home/ricardoa/UProjects/rust/leetcode/add-two-numbers/target/debug/deps/add_two_numbers-b7bcbf2095716efd: src/lib.rs
-
-/home/ricardoa/UProjects/rust/leetcode/add-two-numbers/target/debug/deps/add_two_numbers-b7bcbf2095716efd.d: src/lib.rs
-
-src/lib.rs:
--- add-two-numbers/target/rls/debug/deps/add_two_numbers-5dde41cc7a67c876.d
+++
@@ -1,5 +0,0 @@
-/home/ricardoa/UProjects/rust/leetcode/add-two-numbers/target/rls/debug/deps/add_two_numbers-5dde41cc7a67c876.rmeta: src/lib.rs
-
-/home/ricardoa/UProjects/rust/leetcode/add-two-numbers/target/rls/debug/deps/add_two_numbers-5dde41cc7a67c876.d: src/lib.rs
-
-src/lib.rs:
